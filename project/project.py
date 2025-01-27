9827b1010a93046232a97212d3eb3646a9f7d80e
Gustavo Fernandes <G00425776@atu.ie>
2025-01-27 21:05:16 +0000

    final commit

--- project/project.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the data
-
-weatherdata = pd.read_csv('C:\Users\gusgo\PFDA\PFDA\project\weatherreadings2.csv')  # read the data from the csv file
-
-# Display the first few rows of the dataframe
-weatherdata.head()
